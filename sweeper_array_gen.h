0fb3ae469f2ea4e2e3424d7cab0eef7a8c769af2
Jason Ma <mazixu2004@gmail.com>
2026-07-08 23:11:46 -0700

    Refactor header guard to SWEEPER_ARRAY_GEN_H

--- sweeper_array_gen.h
+++ sweeper_array_gen.h
@@ -7,11 +7,7 @@
 
 class MineSweeperGenerator {
 private:
-    static constexpr std::uint8_t MASK_MINE_COUNT = 0x0F; // mask for number mines around, ex: int (a & MASK_MINE_COUNT): num mines around
-    static constexpr std::uint8_t STATE_REVEALED = 0x10; // mask for whether block is revealed, ex: ((a & STATE_REVEALED) != 0): true if revealed
-    static constexpr std::uint8_t STATE_FLAGGED  = 0x20; // mask for whether block is flagged, ex: int ((a & STATE_FLAGGED) != 0): true if flagged
-    static constexpr std::uint8_t IS_MINE        = 9; // mask for whether block is mine, ex: int ((a & MASK_MINE_COUNT) == IS_MINE): true if is mine
-
+    
     int height; 
     int width; 
     int totalMines;
@@ -21,6 +17,11 @@ private:
     void layMines(std::vector<std::uint8_t>& rawGrid);
 
 public:
+    static constexpr std::uint8_t MASK_MINE_COUNT = 0x0F; // mask for number mines around, ex: int (a & MASK_MINE_COUNT): num mines around
+    static constexpr std::uint8_t STATE_REVEALED = 0x10; // mask for whether block is revealed, ex: ((a & STATE_REVEALED) != 0): true if revealed
+    static constexpr std::uint8_t STATE_FLAGGED  = 0x20; // mask for whether block is flagged, ex: int ((a & STATE_FLAGGED) != 0): true if flagged
+    static constexpr std::uint8_t IS_MINE        = 9; // mask for whether block is mine, ex: int ((a & MASK_MINE_COUNT) == IS_MINE): true if is mine
+
     MineSweeperGenerator(int h, int w, int mines, int firstY, int firstX);
     
     std::unique_ptr<std::vector<std::uint8_t>> generateBoard();
